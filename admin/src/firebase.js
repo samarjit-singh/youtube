f394f0bcd1161e3723b65ea85dbb1f4e250d13eb
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2023-01-08 03:19:23 +0530

    create functionality added from admin pannel

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCB-h_T2ZL-a7xt5zMvZmSh8GBXtawzL0c",
-  authDomain: "shop-889c6.firebaseapp.com",
-  projectId: "shop-889c6",
-  storageBucket: "shop-889c6.appspot.com",
-  messagingSenderId: "105110075028",
-  appId: "1:105110075028:web:7cee5adf1ed39f597e6681",
+  apiKey: "AIzaSyCqXbaR2asajVAHoc47ubEeaTaT504LuR4",
+  authDomain: "pugu-40d26.firebaseapp.com",
+  projectId: "pugu-40d26",
+  storageBucket: "pugu-40d26.appspot.com",
+  messagingSenderId: "428196424637",
+  appId: "1:428196424637:web:66686402effe0e87ba80d4",
 };
 
 // Initialize Firebase
--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -7,14 +7,87 @@ import { useSelector } from "react-redux";
 import { useEffect, useMemo, useState } from "react";
 import { userRequest } from "../../requestMethods";
 
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+import app from "../../firebase";
+
+import { updateProduct } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
+
 export default function Product() {
   const location = useLocation();
   const productId = location.pathname.split("/")[2];
   const [pStats, setPStats] = useState([]);
+  const dispatch = useDispatch();
 
   const product = useSelector((state) =>
     state.product.products.find((product) => product._id === productId)
   );
+
+  const [inputs, setInputs] = useState({});
+  const [files, setFiles] = useState(null);
+  // const [cat, setCat] = useState([]);
+
+  console.log(files);
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const fileName = files.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, files);
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const product = { ...inputs, img: downloadURL };
+          updateProduct(product._id, product, dispatch);
+        });
+      }
+    );
+  };
+
+  console.log(files);
 
   const MONTHS = useMemo(
     () => [
@@ -38,9 +111,9 @@ export default function Product() {
     const getStats = async () => {
       try {
         const res = await userRequest.get("orders/income?pid=" + productId);
-        const list = res.data.sort((a,b)=>{
-            return a._id - b._id
-        })
+        const list = res.data.sort((a, b) => {
+          return a._id - b._id;
+        });
         list.map((item) =>
           setPStats((prev) => [
             ...prev,
@@ -91,13 +164,28 @@ export default function Product() {
         <form className="productForm">
           <div className="productFormLeft">
             <label>Product Name</label>
-            <input type="text" placeholder={product.title} />
+            <input
+              name="title"
+              type="text"
+              placeholder={product.title}
+              onChange={handleChange}
+            />
             <label>Product Description</label>
-            <input type="text" placeholder={product.desc} />
+            <input
+              name="desc"
+              type="text"
+              placeholder={product.desc}
+              onChange={handleChange}
+            />
             <label>Price</label>
-            <input type="text" placeholder={product.price} />
+            <input
+              name="price"
+              type="number"
+              placeholder={product.price}
+              onChange={handleChange}
+            />
             <label>In Stock</label>
-            <select name="inStock" id="idStock">
+            <select name="inStock" id="idStock" onChange={handleChange}>
               <option value="true">Yes</option>
               <option value="false">No</option>
             </select>
@@ -108,9 +196,16 @@ export default function Product() {
               <label for="file">
                 <Publish />
               </label>
-              <input type="file" id="file" style={{ display: "none" }} />
+              <input
+                type="file"
+                id="file"
+                onChange={(e) => setFiles(e.target.files[0])}
+                style={{ display: "none" }}
+              />
             </div>
-            <button className="productButton">Update</button>
+            <button onClick={handleClick} className="productButton">
+              Update
+            </button>
           </div>
         </form>
       </div>
